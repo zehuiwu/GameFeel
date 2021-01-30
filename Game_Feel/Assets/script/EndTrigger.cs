a48630a3bb116f0313c00f2424cd7b13f2a0bf40
zehui wu <zehuiwu@usc.edu>
2021-01-30 16:58:23 +0800

    first commit

---
+++ Game_Feel/Assets/script/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
